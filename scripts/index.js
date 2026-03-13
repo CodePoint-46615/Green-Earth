46bc8d0ea4b3d96dc66aed21bcfe9bc36a323f8f
CodePoint-46615 <codepoint46615@gmail.com>
2026-03-14 02:34:03 +0600

    categorites menu

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+const loadCategory = () => {
+    const url = "https://openapi.programming-hero.com/api/categories";
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            // console.log(data.categories); 
+            displayCategory(data.categories);
+        });
+}
+
+// {
+//     "id": 1,
+//     "category_name": "Fruit Tree",
+//     "small_description": "Trees that bear edible fruits like mango, guava, and jackfruit."
+// }
+
+displayCategory = (allCategoryData) => {
+    // console.log(allCategoryData); 
+    const parentContainer = document.getElementById("choose-tree-menu");
+    parentContainer.innerHTML =
+        `
+        <li class="menu bg-base-200 rounded-box w-20 md:w-56">
+        <a>All Trees</a>
+        </li>
+        
+    `;
+
+    allCategoryData.forEach(element => {
+        const id = element.category_name;
+
+        const childElement = document.createElement("ul");
+        childElement.innerHTML =
+        `
+        <ul class="menu bg-base-200 rounded-box w-30 md:w-56">
+            <li class=""><a>${id}</a></li>
+        </ul>
+        
+        `;
+
+        parentContainer.appendChild(childElement);
+    });
+}
+
+loadCategory(); 
